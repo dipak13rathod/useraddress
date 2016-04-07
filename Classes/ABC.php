544042b1fa07312b106825442f2d2fd1c48e62ef
dipak13rathod <dipak.rathod1992@gmail.com>
2016-04-07 17:16:41 +0530

    File XYZ used in COMBINE.php

---
+++ Classes/ABC.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Dipak
+ * Date: 4/7/2016
+ * Time: 4:27 PM
+ */
+class ABC{
+    public $Name="Jay";
+    public $Surname="Joshi";
+
+    public  function a(){
+
+        echo "My Name Is ".$this->Name." ".$this->Surname.".<br>";
+    }
+}
