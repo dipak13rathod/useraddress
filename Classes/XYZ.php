41d19ae3fd47bd73eb33f356ac7b07702c47614a
dipak13rathod <dipak.rathod1992@gmail.com>
2016-04-07 17:18:23 +0530

    File XYZ used in COMBINE.php

---
+++ Classes/XYZ.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Dipak
+ * Date: 4/7/2016
+ * Time: 4:20 PM
+ */
+class XYZ{
+    public $Name="Dipak";
+    public $Surname="Rathod";
+
+    public function x(){
+
+        echo "My Name Is ".$this->Name." ".$this->Surname.".<br>";
+    }
+}
+
